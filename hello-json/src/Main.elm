6536bf958aa4f2dc8b5d3239d1fe3e2c74ed8151
Krishna Addepalli <coolkrishna31@gmail.com>
2024-11-04 15:50:33 +0100

    Flesh out a simple JSON Api call and display the title

--- hello-json/src/Main.elm
+++ hello-json/src/Main.elm
@@ -1,12 +1,46 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser
+import Html
 import Http
 import Json.Decode
 
 
+main : Program () Model Msg
 main =
-    text "hi"
+    Browser.element
+        { init = initModel
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+initModel : () -> ( Model, Cmd Msg )
+initModel _ =
+    ( { title = "Testing" }, getTitle )
+
+
+view : Model -> Html.Html msg
+view model =
+    Html.text model.title
+
+
+update : Msg -> Model -> ( Model, Cmd msg )
+update msg model =
+    case msg of
+        MsgGotTitle result ->
+            case result of
+                Ok data ->
+                    ( { model | title = data }, Cmd.none )
+
+                Err _ ->
+                    ( { model | title = "Error" }, Cmd.none )
+
+
+subscriptions : Model -> Sub msg
+subscriptions _ =
+    Sub.none
 
 
 type alias Model =
@@ -18,12 +52,14 @@ type Msg
     = MsgGotTitle (Result Http.Error String)
 
 
+getTitle : Cmd Msg
 getTitle =
     Http.get
-        { url = "https://jsonplaceholder.typicode.com/posts/1"
+        { url = "https://jsonplaceholder.typicode.com/posts/2"
         , expect = Http.expectJson MsgGotTitle dataTitleDecoder
         }
 
 
+dataTitleDecoder : Json.Decode.Decoder String
 dataTitleDecoder =
     Json.Decode.field "title" Json.Decode.string
