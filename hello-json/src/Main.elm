976f7526ddc019d602a70b51095c9cfb68790e0f
Krishna Addepalli <coolkrishna31@gmail.com>
2024-11-04 15:25:04 +0100

    Initial json title decoder from the url

--- hello-json/src/Main.elm
+++ hello-json/src/Main.elm
@@ -1,5 +1,29 @@
 module Main exposing (main)
 
 import Html exposing (text)
+import Http
+import Json.Decode
 
-main = text "hi"
+
+main =
+    text "hi"
+
+
+type alias Model =
+    { title : String
+    }
+
+
+type Msg
+    = MsgGotTitle (Result Http.Error String)
+
+
+getTitle =
+    Http.get
+        { url = "https://jsonplaceholder.typicode.com/posts/1"
+        , expect = Http.expectJson MsgGotTitle dataTitleDecoder
+        }
+
+
+dataTitleDecoder =
+    Json.Decode.field "title" Json.Decode.string
