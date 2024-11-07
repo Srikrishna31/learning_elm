f22767b7baf8fd13a23f94037b041d6be35ccddf
Krishna Addepalli <coolkrishna31@gmail.com>
2024-11-07 21:41:05 +0100

    Add a simple button to increase the counter

--- counter/src/Main.elm
+++ counter/src/Main.elm
@@ -2,9 +2,10 @@ module Main exposing (main)
 
 import Browser exposing (Document)
 import Html exposing (text)
+import Html.Events exposing (onClick)
 
 
-main : Program () Model msg
+main : Program () Model Msg
 main =
     Browser.document
         { init = init
@@ -14,15 +15,20 @@ main =
         }
 
 
-view : Model -> Document msg
+view : Model -> Document Msg
 view model =
     { title = model.title
-    , body = [ Html.text "Hello Document" ]
+    , body = [ Html.button [ onClick MsgIncreaseCounter ] [ Html.text ("Counter " ++ String.fromInt model.counter) ] ]
     }
+
+
+type Msg
+    = MsgIncreaseCounter
 
 
 type alias Model =
     { title : String
+    , counter : Int
     }
 
 
@@ -34,12 +40,25 @@ init _ =
 initModel : Model
 initModel =
     { title = "My title"
+    , counter = 0
     }
 
 
-update : msg -> Model -> ( Model, Cmd msg )
-update _ model =
-    ( model, Cmd.none )
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        MsgIncreaseCounter ->
+            let
+                newCounter =
+                    model.counter + 1
+
+                newTitle =
+                    "My Title" ++ String.fromInt newCounter
+
+                newModel =
+                    { model | counter = newCounter, title = newTitle }
+            in
+            ( newModel, Cmd.none )
 
 
 subscriptions : Model -> Sub msg
