86d6f69773d40185626a6dca73585c340ea28bcd
Krishna Addepalli <coolkrishna31@gmail.com>
2024-11-08 15:19:56 +0100

    Add initial project files for Fuzz

---
+++ fuzz/src/Main.elm
@@ -0,0 +1,21 @@
+module Main exposing (main)
+
+import Html exposing (text)
+
+
+main : Html.Html msg
+main =
+    text (shrinkText 120 "Hello Fuzz this is a longer string")
+
+
+shrinkText : Int -> String -> String
+shrinkText max text =
+    if String.length text <= max then
+        text
+
+    else
+        let
+            extra =
+                String.length text - max
+        in
+        String.dropRight extra text ++ " ..."
