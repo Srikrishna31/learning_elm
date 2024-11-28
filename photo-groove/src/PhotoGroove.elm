add1fbe0ba809340f7049871d3c9d03ed0811042
Krishna Addepalli <coolkrishna31@gmail.com>
2024-11-28 15:21:36 +0100

    Initial working PhotoGroove application

--- photo-groove/src/PhotoGroove.elm
+++ photo-groove/src/PhotoGroove.elm
@@ -1,7 +1,26 @@
 module PhotoGroove exposing (main)
 
+import Browser
 import Html exposing (div, h1, img, text)
 import Html.Attributes as Html exposing (..)
+import Html.Events exposing (onClick)
+
+
+
+{-
+   The `Browser.sandbox` function takes a record with three fields:
+       * model - A value that can be anything you please
+       * view - A function that takes a model and returns an Html node
+       * update - A function that takes a message and a model, and return a new model
+-}
+
+
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
 
 
 urlPrefix =
@@ -12,7 +31,7 @@ view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            (List.map (\photo -> viewThumbnail model.selectedUrl photo) model.photos)
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img
             [ class "large"
             , src (urlPrefix ++ "large/" ++ model.selectedUrl)
@@ -25,6 +44,7 @@ viewThumbnail selectedUrl thumb =
     img
         [ src (urlPrefix ++ thumb.url)
         , Html.classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick { description = "ClickedPhoto", data = thumb.url }
         ]
         []
 
@@ -39,5 +59,29 @@ initialModel =
     }
 
 
-main =
-    view initialModel
+
+{-
+   In general, when update function receives a message, it will do the following:
+   1. Look at the message  it received
+   2. Look at our current model
+   3. Use these two values to determine a new model, and then return it.
+-}
+
+
+update msg model =
+    if msg.description == "ClickedPhoto" then
+        { model | selectedUrl = msg.data }
+
+    else
+        model
+
+
+
+{-
+   1. A model represents our application state.
+   2. A view function takes a model and returns a list of Html nodes.
+   3. User events such as clicks get translated into message values.
+   4. Messages get run through the update function to produce a new model.
+   5. After an update, the new model is sent to the view function to determine the new DOM.
+   6. Browser.sandbox wires together model, view and update.
+-}
