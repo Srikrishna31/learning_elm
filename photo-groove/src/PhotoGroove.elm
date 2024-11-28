add3fd5e26bf299d28068167c1a5679f3d57d13f
Krishna Addepalli <coolkrishna31@gmail.com>
2024-11-28 06:15:47 +0100

    Add initial model to store the thumbnail photos

--- photo-groove/src/PhotoGroove.elm
+++ photo-groove/src/PhotoGroove.elm
@@ -4,16 +4,28 @@ import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
 
+urlPrefix =
+    "https://elm-in-action.com/"
+
+
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            [ img [ src "https://elm-in-action.com/1.jpeg" ] []
-            , img [ src "https://elm-in-action.com/2.jpeg" ] []
-            , img [ src "https://elm-in-action.com/3.jpeg" ] []
-            ]
+            (List.map viewThumbnail model)
         ]
 
 
+viewThumbnail thumb =
+    img [ src (urlPrefix ++ thumb.url) ] []
+
+
+initialModel =
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
+
+
 main =
-    view "no model yet"
+    view initialModel
