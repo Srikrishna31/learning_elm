f111e08c6e47e2e124bede29521ac7e192cdf5b7
Krishna Addepalli <coolkrishna31@gmail.com>
2024-11-28 13:25:19 +0100

    Show selected image enlarged

--- photo-groove/src/PhotoGroove.elm
+++ photo-groove/src/PhotoGroove.elm
@@ -1,7 +1,7 @@
 module PhotoGroove exposing (main)
 
 import Html exposing (div, h1, img, text)
-import Html.Attributes exposing (..)
+import Html.Attributes as Html exposing (..)
 
 
 urlPrefix =
@@ -12,19 +12,31 @@ view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            (List.map viewThumbnail model)
+            (List.map (\photo -> viewThumbnail model.selectedUrl photo) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
         ]
 
 
-viewThumbnail thumb =
-    img [ src (urlPrefix ++ thumb.url) ] []
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , Html.classList [ ( "selected", selectedUrl == thumb.url ) ]
+        ]
+        []
 
 
 initialModel =
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
 
 
 main =
