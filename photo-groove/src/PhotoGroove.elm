bc4ee1b75b0c51ebf99826b51abb1d3983c4a073
Krishna Addepalli <coolkrishna31@gmail.com>
2024-11-29 06:18:58 +0100

    Add a random choice on Surprise Me button click

--- photo-groove/src/PhotoGroove.elm
+++ photo-groove/src/PhotoGroove.elm
@@ -5,6 +5,7 @@ import Browser
 import Html exposing (Html, button, div, h1, h3, img, input, label, text)
 import Html.Attributes as Html exposing (..)
 import Html.Events exposing (onClick)
+import Random
 
 
 
@@ -14,13 +15,28 @@ import Html.Events exposing (onClick)
        * view - A function that takes a model and returns an Html node
        * update - A function that takes a message and a model, and return a new model
 -}
+{-
+   The () value is known as unit. It contains no information whatsoever. It's both a value
+   and a type; the () type can be satisfied only with the () value. An example:
+
+    getUltimateAnswer: () -> Int
+    getUltimateAnswer unit = 40 + 2
+
+    The only way to call getUltimateAnswer would be to pass () to it; because it accepts
+    () as an argument, no other value but () will do.
+
+    Putting it together, we can read Program () Model Msg as "an Elm program with no flags, whose
+    model type is Model and whose message type is Msg".
+-}
 
 
+main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initialModel
+    Browser.element
+        { init = \_ -> ( initialModel, Cmd.none )
         , view = view
         , update = update
+        , subscriptions = \_ -> Sub.none
         }
 
 
@@ -129,6 +145,7 @@ type alias Model =
 type Msg
     = ClickedPhoto String
     | ClickedSize ThumbnailSize
+    | GotSelectedIndex Int
     | ClickedSurpriseMe
 
 
@@ -145,24 +162,57 @@ photoArray =
 
 
 {-
+   Because Elm forbids functions from returning different values when they receive
+   the same arguments, it's not possible to write an inconsistent function like Math.random
+   as a plain Elm function. Instead, Elm implements random number generation by using a command.
+
+    A command is a value that describes an operation for Elm runtime to perform. Unlike calling
+    a function, running the same command multiple times can have a different result.
+-}
+
+
+randomPhotoPicker : Random.Generator Int
+randomPhotoPicker =
+    Random.int 0 (Array.length photoArray - 1)
+
+
+
+{-
    In general, when update function receives a message, it will do the following:
    1. Look at the message  it received
    2. Look at our current model
    3. Use these two values to determine a new model, and then return it.
+
+   Because update returns a tuple of a new Model and a command, it's possible for the
+   same update branch to change the Model and to run a command as well. In such a case,
+   first Model would change, then view would get run on the new Model to update the page,
+   and finally the command would run.
 -}
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ClickedPhoto url ->
-            { model | selectedUrl = url }
+            ( { model | selectedUrl = url }, Cmd.none )
 
         ClickedSize size ->
-            { model | chosenSize = size }
+            ( { model | chosenSize = size }, Cmd.none )
 
+        {-
+           We could write the type annotation for Random.generate like so:
+           generate : (randomValue -> msg) -> Random.Generator randomValue -> Cmd msg
+
+           Capitalization is important. There is a big difference between Msg and msg here.
+           A function that returns Msg returns an instance of exact Msg custom type defined above.
+           In contrast, msg is a type variable. A function that returns msg could return anything
+           at all.
+        -}
         ClickedSurpriseMe ->
-            { model | selectedUrl = "2.jpeg" }
+            ( model, Random.generate GotSelectedIndex randomPhotoPicker )
+
+        GotSelectedIndex index ->
+            ( { model | selectedUrl = getPhotoUrl index }, Cmd.none )
 
 
 
