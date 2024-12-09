b9ec48a58e4a336e9d6dacc885db9e8b0edaeb0e
Krishna Addepalli <coolkrishna31@gmail.com>
2024-12-09 21:23:40 +0100

    Add code for rendering a navigation bar

--- query-plan-viewer/src/Main.elm
+++ query-plan-viewer/src/Main.elm
@@ -1,7 +1,8 @@
 module Main exposing (main)
 
 import Browser
-import Html
+import Element exposing (..)
+import Element.Border as Border
 
 
 type Page
@@ -56,8 +57,32 @@ update msg model =
 view : Model -> Browser.Document Msg
 view model =
     { title = "VisExp"
-    , body = []
+    , body =
+        [ layout [] <|
+            column [ width fill, spacingXY 0 20 ]
+                [ navBar
+                , el [ centerX ] <| text "VisExp"
+                ]
+        ]
     }
+
+
+blue : Color
+blue =
+    rgb255 52 101 164
+
+
+navBar : Element Msg
+navBar =
+    row
+        [ width fill
+        , paddingXY 60 10
+        , Border.widthEach { bottom = 5, top = 0, left = 0, right = 0 }
+        , Border.color blue
+        ]
+        [ el [ alignLeft ] <| text "VisExp"
+        , el [ alignRight ] <| text "Menu"
+        ]
 
 
 main : Program Flags Model Msg
