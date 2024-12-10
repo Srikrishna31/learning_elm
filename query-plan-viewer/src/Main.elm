2d6ee900c06cc46c99e91979ea213dab118cb1ae
Krishna Addepalli <coolkrishna31@gmail.com>
2024-12-10 05:26:43 +0100

    Add code for input and display pages

--- query-plan-viewer/src/Main.elm
+++ query-plan-viewer/src/Main.elm
@@ -2,19 +2,25 @@ module Main exposing (main)
 
 import Browser
 import Element exposing (..)
+import Element.Background as Background
 import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
 
 
 type Page
     = InputPage
+    | DisplayPage
 
 
 type Msg
-    = NoOp
+    = SubmitPlan
+    | ChangePlanText String
 
 
 type alias Model =
     { currPage : Page
+    , currPlanText : String
     }
 
 
@@ -25,6 +31,7 @@ type alias Flags =
 init : Flags -> ( Model, Cmd Msg )
 init _ =
     ( { currPage = InputPage
+      , currPlanText = ""
       }
     , Cmd.none
     )
@@ -46,8 +53,11 @@ subscriptions model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        _ ->
-            ( model, Cmd.none )
+        ChangePlanText s ->
+            ( { model | currPlanText = s }, Cmd.none )
+
+        SubmitPlan ->
+            ( { model | currPage = DisplayPage }, Cmd.none )
 
 
 
@@ -56,12 +66,21 @@ update msg model =
 
 view : Model -> Browser.Document Msg
 view model =
+    let
+        content =
+            case model.currPage of
+                InputPage ->
+                    inputPage model
+
+                DisplayPage ->
+                    displayPage model
+    in
     { title = "VisExp"
     , body =
         [ layout [] <|
             column [ width fill, spacingXY 0 20 ]
                 [ navBar
-                , el [ centerX ] <| text "VisExp"
+                , content
                 ]
         ]
     }
@@ -70,6 +89,71 @@ view model =
 blue : Color
 blue =
     rgb255 52 101 164
+
+
+lightCharcoal : Color
+lightCharcoal =
+    rgb255 136 138 133
+
+
+green : Color
+green =
+    rgb255 0 255 0
+
+
+white : Color
+white =
+    rgb255 255 255 255
+
+
+darkGreen : Color
+darkGreen =
+    rgb255 0 150 0
+
+
+inputPage : Model -> Element Msg
+inputPage model =
+    column
+        [ width (px 800)
+        , spacingXY 0 10
+        , centerX
+        ]
+        [ Input.multiline
+            [ height (px 300)
+            , Border.width 1
+            , Border.rounded 3
+            , Border.color lightCharcoal
+            , padding 3
+            ]
+            { onChange = ChangePlanText
+            , text = model.currPlanText
+            , placeholder = Nothing
+            , label =
+                Input.labelAbove [] <|
+                    text "Paste the EXPLAIN output in JSON format:"
+            , spellcheck = False
+            }
+        , Input.button
+            [ Background.color green
+            , Border.color darkGreen
+            , Border.rounded 3
+            , Border.widthEach { bottom = 3, top = 0, right = 0, left = 0 }
+            , Font.bold
+            , Font.color white
+            , paddingXY 20 6
+            , alignRight
+            , width (px 200)
+            , height (px 40)
+            ]
+            { onPress = Just SubmitPlan
+            , label = el [ centerX ] <| text "Go!"
+            }
+        ]
+
+
+displayPage : Model -> Element Msg
+displayPage model =
+    column [] [ text model.currPlanText ]
 
 
 navBar : Element Msg
