f659965c9ec2bca3871ece36fdb5426530e50b3e
Krishna Addepalli <coolkrishna31@gmail.com>
2024-12-16 21:53:57 +0100

    Add a simple fuzz test for update function test

--- query-plan-viewer/tests/Tests.elm
+++ query-plan-viewer/tests/Tests.elm
@@ -1,10 +1,13 @@
 module Tests exposing (..)
 
+import Auth exposing (Msg(..))
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Json.Decode
+import Main exposing (Msg(..), init, update)
 import PlanParsers.Json exposing (SavedPlan, decodeSavedPlans)
 import Test exposing (..)
+import TestUpdate
 
 
 suite : Test
@@ -69,6 +72,23 @@ suite =
         ]
 
 
+suite1 : Test
+suite1 =
+    let
+        ( model, _ ) =
+            init { sessionId = Just "123" }
+    in
+    describe "Update function"
+        [ TestUpdate.fuzz update
+            msgFuzzer
+            model
+            "Should not reset session ID Wrongly"
+          <|
+            \mdl ->
+                mdl.appState.auth.sessionId |> Expect.notEqual Nothing
+        ]
+
+
 expectSortedVersions : SavedPlan -> Expectation
 expectSortedVersions plan =
     let
@@ -80,3 +100,19 @@ expectSortedVersions plan =
 
     else
         Expect.fail "Versions are not sorted"
+
+
+msgFuzzer : Fuzz.Fuzzer Main.Msg
+msgFuzzer =
+    Fuzz.oneOf <|
+        List.map Fuzz.constant
+            [ Auth <| ChangePassword "a"
+            , Auth <| ChangeUserName "a"
+            , Auth <| StartLogin
+            , ChangePlanText "{}"
+            , CreatePlan
+            , RequestLogin
+            , RequestRegistration
+            , SubmitPlan
+            , ToggleMenu
+            ]
