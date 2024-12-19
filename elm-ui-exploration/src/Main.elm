721de6885ce6e74fbcf02b9e8a5569d7e5eb21e2
Krishna Addepalli <coolkrishna31@gmail.com>
2024-12-19 09:18:21 +0100

    Initial rendering of a basic chat panel

--- elm-ui-exploration/src/Main.elm
+++ elm-ui-exploration/src/Main.elm
@@ -5,6 +5,7 @@ import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
+import Element.Input as Input
 import Html exposing (Html)
 
 
@@ -41,14 +42,65 @@ menu =
         ]
 
 
+type alias Message =
+    { author : String, time : String, text : String }
+
+
+messageList : List Message
+messageList =
+    [ { author = "augustin82"
+      , time = "6:09AM"
+      , text =
+            """
+            @gampleman I think you need to `clip` the `scrollable` element, and that element should be larger than its
+            parent, which (I think) means that the containing parent should have a fixed width
+            """
+      }
+    , { author = "u0421793"
+      , time = "6:22AM"
+      , text =
+            """
+            I've been trying to make a few links on a page in elm and elm-ui but I've not found a way to make it work
+            because I haven't found any examples of elm-ui which incorporate an anchor element
+            """
+      }
+    , { author = "augustin82"
+      , time = "6:27AM"
+      , text =
+            """
+            @u0421793 what are you looking for exactly? do you have an Ellie where you've tried doing some stuff?
+            """
+      }
+    , { author = "icepac"
+      , time = "7:53AM"
+      , text =
+            """
+            Anybody replied to @lango
+            """
+      }
+    ]
+
+
+channelList : List String
+channelList =
+    [ "ellie"
+    , "elm-dev"
+    , "elm-discuss"
+    , "elm-format"
+    , "elm-ui"
+    , "general"
+    , "news-and-links"
+    ]
+
+
 main : Html msg
 main =
     layout
         [ width fill, height fill ]
     <|
         row [ height fill, width fill ]
-            [ channelPanel
-            , chatPanel
+            [ channelPanel channelList "elm-ui"
+            , chatPanel "elm-ui" messageList
             ]
 
 
@@ -57,18 +109,92 @@ main =
 -- the sibling elements
 
 
-channelPanel : Element msg
-channelPanel =
+channelPanel : List String -> String -> Element msg
+channelPanel channels activeChannel =
+    let
+        activeChannelAttrs =
+            [ Background.color <| rgb255 117 179 201, Font.bold ]
+
+        channelAttrs =
+            [ paddingXY 15 5, width fill ]
+
+        channelEl channel =
+            el
+                (if channel == activeChannel then
+                    activeChannelAttrs ++ channelAttrs
+
+                 else
+                    channelAttrs
+                )
+            <|
+                text ("# " ++ channel)
+    in
     column
         [ height fill
         , width <| fillPortion 1
         , Background.color <| rgb255 92 99 118
         , Font.color <| rgb255 255 255 255
         ]
-        [ text "channels" ]
+    <|
+        List.map channelEl channels
 
 
-chatPanel : Element msg
-chatPanel =
+chatPanel : String -> List Message -> Element msg
+chatPanel channel messages =
+    let
+        header =
+            row
+                [ width fill
+                , paddingXY 20 5
+                , Border.widthEach { bottom = 1, top = 0, left = 0, right = 0 }
+                , Border.color <| rgb255 200 200 200
+                ]
+                [ el [] <| text ("#" ++ channel)
+                , Input.button
+                    [ padding 5
+                    , alignRight
+                    , Border.width 1
+                    , Border.rounded 3
+                    , Border.color <| rgb255 200 200 200
+                    ]
+                    { onPress = Nothing
+                    , label = text "Search"
+                    }
+                ]
+
+        messageEntry message =
+            column [ width fill, spacingXY 0 5 ]
+                [ row [ spacingXY 10 0 ]
+                    [ el [ Font.bold ] <| text message.author, text message.time ]
+                , paragraph [] [ text message.text ]
+                ]
+
+        messagePanel =
+            column [ padding 10, spacingXY 0 20, scrollbarY ] <|
+                List.map messageEntry messages
+
+        footer =
+            el [ alignBottom, padding 20, width fill ] <|
+                row
+                    [ spacingXY 2 0
+                    , width fill
+                    , Border.width 2
+                    , Border.rounded 4
+                    , Border.color <| rgb255 200 200 200
+                    ]
+                    [ el
+                        [ padding 5
+                        , Border.widthEach { right = 2, left = 0, top = 0, bottom = 0 }
+                        , Border.color <| rgb255 200 200 200
+                        , mouseOver [ Background.color <| rgb255 86 182 139 ]
+                        ]
+                      <|
+                        text "+"
+                    , el [ Background.color <| rgb255 255 255 255 ] none
+                    ]
+    in
     column [ height fill, width <| fillPortion 5 ]
-        [ text "chat" ]
+        [ header
+        , messagePanel
+        , footer
+        ]
