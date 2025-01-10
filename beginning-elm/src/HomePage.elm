821150c9773a013a9bf3751891608afe1ec6ec3b
Krishna Addepalli <coolkrishna31@gmail.com>
2025-01-10 16:23:09 +0100

    Add Regex dependency

--- beginning-elm/src/HomePage.elm
+++ beginning-elm/src/HomePage.elm
@@ -2,8 +2,10 @@ module HomePage exposing (main)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Regex exposing (Regex)
 
 
+view : String -> Html msg
 view model =
     div [ class "jumbotron" ]
         [ h1 [] [ text "Welcome to Dunder Mifflin!" ]
@@ -17,8 +19,43 @@ view model =
                 small-business clients.
                 """
             ]
+        , p []
+            [ text <| "Regex.contains regex apollo11 " ++ (fromBool <| Regex.contains regex apollo11) ]
         ]
 
 
+main : Html msg
 main =
     view "dummy model"
+
+
+pattern : String
+pattern =
+    "\\d\\d:\\d\\d (a\\.m\\.|p\\.m\\.)"
+
+
+maybeRegex : Maybe Regex
+maybeRegex =
+    Regex.fromString pattern
+
+
+regex : Regex
+regex =
+    Maybe.withDefault Regex.never maybeRegex
+
+
+apollo11 : String
+apollo11 =
+    """
+            On July 16, 1969, the massive Saturn V rocket lifted off from NASA's Kennedy Space Center at 09:32 a.m. EDT.
+            Four days later, on July 20, Neil Armstrong and BUzz Aldrin landed on the Moon. 
+           """
+
+
+fromBool : Bool -> String
+fromBool value =
+    if value then
+        "True"
+
+    else
+        "False"
