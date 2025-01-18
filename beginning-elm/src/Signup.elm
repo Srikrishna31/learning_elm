b28ef824fead1e0f746cc9cada56f1aefdc3bee8
Krishna Addepalli <coolkrishna31@gmail.com>
2025-01-18 07:09:13 +0100

    Render a simple Signup page with MVU Elm architecture

--- beginning-elm/src/Signup.elm
+++ beginning-elm/src/Signup.elm
@@ -1,18 +1,20 @@
 module Signup exposing (..)
 
+--import Html exposing (Attribute, Html, button, div, h1, input, label, text)
+--import Html.Attributes exposing (class, for, id, style, type_)
+
 import Browser
-import Html exposing (Attribute, Html, button, div, h1, input, label, text)
-import Html.Attributes exposing (class, for, id, style, type_)
-
-
-
---import Css exposing (..)
---import Html.Styled exposing (..)
---import Html.Styled.Attributes exposing (..)
+import Css exposing (..)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (..)
+import Html.Styled.Events exposing (onClick, onInput)
 
 
 type Msg
-    = Dummy
+    = SaveName String
+    | SaveEmail String
+    | SavePassword String
+    | Signup
 
 
 type alias User =
@@ -43,63 +45,30 @@ initialModel =
 
       The best practice for working with HTML suggests that the styles should primarily be specified in CSS files.
       Therefore, we shouldn't use too many inline styles.
+
 -}
 
 
-view : User -> Html msg
+view : User -> Html Msg
 view user =
-    div [ class "container" ]
-        [ div [ class "row" ]
-            [ div [ class "col-md-6 col-md-offset-3" ]
-                [ h1 [ class "text-center" ] [ text "Sign Up" ]
-                , Html.form []
-                    [ div [ class "form-group" ]
-                        [ label
-                            [ class "control-label"
-                            , for "name"
-                            ]
-                            [ text "Name" ]
-                        , input
-                            [ class "form-control"
-                            , id "name"
-                            , type_ "text"
-                            ]
-                            []
-                        ]
-                    , div [ class "form-group" ]
-                        [ label
-                            [ class "control-label"
-                            , for "email"
-                            ]
-                            [ text "Email" ]
-                        , input
-                            [ class "form-control"
-                            , id "email"
-                            , type_ "email"
-                            ]
-                            []
-                        ]
-                    , div [ class "form-group" ]
-                        [ label
-                            [ class "control-label"
-                            , for "password"
-                            ]
-                            [ text "Password" ]
-                        , input
-                            [ class "form-control"
-                            , id "password"
-                            , type_ "password"
-                            ]
-                            []
-                        ]
-                    , div [ class "text-center" ]
-                        [ button
-                            [ class "btn btn-lg btn-primary"
-                            , type_ "submit"
-                            ]
-                            [ text "Create my account" ]
-                        ]
-                    ]
+    div []
+        [ h1 [ css [ paddingLeft (cm 3) ] ] [ text "Sign up" ]
+        , styledForm []
+            [ div []
+                [ text "Name"
+                , styledInput [ id "name", type_ "text", onInput SaveName ] []
+                ]
+            , div []
+                [ text "Email"
+                , styledInput [ id "email", type_ "email", onInput SaveEmail ] []
+                ]
+            , div []
+                [ text "Password"
+                , styledInput [ id "password", type_ "password", onInput SavePassword ] []
+                ]
+            , div []
+                [ styledButton [ type_ "submit", onClick Signup ]
+                    [ text "Create my account" ]
                 ]
             ]
         ]
@@ -132,40 +101,42 @@ view user =
          -> Html msg
     The first argument is an HTML element from the Html.Styled module. The second argument is a list of CSS styles.
 -}
---styledForm : List (Attribute msg) -> List (Html msg) -> Html msg
---styledForm =
---    styled Html.Styled.form
---        [ borderRadius <| px 5
---        , backgroundColor <| hex "#f2f2f2"
---        , padding <| px 20
---        , Css.width <| px 300
---        ]
---
---
---styledInput : List (Attribute msg) -> List (Html msg) -> Html msg
---styledInput =
---    styled Html.Styled.input
---        [ display block
---        , Css.width <| px 260
---        , padding2 (px 12) <| px 20
---        , margin2 (px 8) <| px 0
---        , border <| px 0
---        , borderRadius <| px 4
---        ]
---
---
---styledButton : List (Attribute msg) -> List (Html msg) -> Html msg
---styledButton =
---    styled Html.Styled.button
---        [ Css.width <| px 300
---        , backgroundColor <| hex "#397cd5"
---        , color <| hex "#fff"
---        , padding2 (px 12) <| px 20
---        , marginTop <| px 10
---        , border <| px 0
---        , borderRadius <| px 4
---        , fontSize <| px 16
---        ]
+
+
+styledForm : List (Attribute msg) -> List (Html msg) -> Html msg
+styledForm =
+    styled Html.Styled.form
+        [ borderRadius <| px 5
+        , backgroundColor <| hex "#f2f2f2"
+        , padding <| px 20
+        , Css.width <| px 300
+        ]
+
+
+styledInput : List (Attribute msg) -> List (Html msg) -> Html msg
+styledInput =
+    styled Html.Styled.input
+        [ display block
+        , Css.width <| px 260
+        , padding2 (px 12) <| px 20
+        , margin2 (px 8) <| px 0
+        , border <| px 0
+        , borderRadius <| px 4
+        ]
+
+
+styledButton : List (Attribute msg) -> List (Html msg) -> Html msg
+styledButton =
+    styled Html.Styled.button
+        [ Css.width <| px 300
+        , backgroundColor <| hex "#397cd5"
+        , color <| hex "#fff"
+        , padding2 (px 12) <| px 20
+        , marginTop <| px 10
+        , border <| px 0
+        , borderRadius <| px 4
+        , fontSize <| px 16
+        ]
 
 
 init : User
@@ -173,15 +144,36 @@ init =
     initialModel
 
 
+
+{-
+   # Using >> operator
+
+   >> is a built-in operator for composing multiple functions. You can think of func1 >> func2 as equivalent of this:
+
+    func1 >> func2 == \param -> func2 (func1 param)
+-}
+
+
 main : Program () User Msg
 main =
     Browser.sandbox
         { init = initialModel
-        , view = view
+        , view = view >> toUnstyled
         , update = update
         }
 
 
 update : Msg -> User -> User
 update msg user =
-    user
+    case msg of
+        SaveName name ->
+            { user | name = name }
+
+        SaveEmail email ->
+            { user | email = email }
+
+        SavePassword password ->
+            { user | password = password }
+
+        Signup ->
+            { user | loggedIn = True }
