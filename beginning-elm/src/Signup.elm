7828e9510dd9668ad9a6f3850f3f86be68ebc3df
Krishna Addepalli <coolkrishna31@gmail.com>
2025-01-18 06:29:19 +0100

    Render signup form using external css file

--- beginning-elm/src/Signup.elm
+++ beginning-elm/src/Signup.elm
@@ -1,12 +1,14 @@
 module Signup exposing (..)
 
---import Html exposing (Attribute, Html, button, div, h1, input, text)
---import Html.Attributes exposing (id, style, type_)
-
 import Browser
-import Css exposing (..)
-import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (..)
+import Html exposing (Attribute, Html, button, div, h1, input, text)
+import Html.Attributes exposing (id, style, type_)
+
+
+
+--import Css exposing (..)
+--import Html.Styled exposing (..)
+--import Html.Styled.Attributes exposing (..)
 
 
 type Msg
@@ -47,22 +49,22 @@ initialModel =
 view : User -> Html msg
 view user =
     div []
-        [ h1 [ css [ paddingLeft <| cm 3 ] ] [ text "Sign up" ]
-        , styledForm []
+        [ h1 [] [ text "Sign up" ]
+        , Html.form []
             [ div []
                 [ text "Name"
-                , styledInput [ id "name", type_ "text" ] []
+                , input [ id "name", type_ "text" ] []
                 ]
             , div []
                 [ text "Email"
-                , styledInput [ id "email", type_ "email" ] []
+                , input [ id "email", type_ "email" ] []
                 ]
             , div []
                 [ text "Password"
-                , styledInput [ id "password", type_ "password" ] []
+                , input [ id "password", type_ "password" ] []
                 ]
             , div []
-                [ styledButton [ type_ "submit" ]
+                [ button [ type_ "submit" ]
                     [ text "Create my account" ]
                 ]
             ]
@@ -96,42 +98,40 @@ view user =
          -> Html msg
     The first argument is an HTML element from the Html.Styled module. The second argument is a list of CSS styles.
 -}
-
-
-styledForm : List (Attribute msg) -> List (Html msg) -> Html msg
-styledForm =
-    styled Html.Styled.form
-        [ borderRadius <| px 5
-        , backgroundColor <| hex "#f2f2f2"
-        , padding <| px 20
-        , Css.width <| px 300
-        ]
-
-
-styledInput : List (Attribute msg) -> List (Html msg) -> Html msg
-styledInput =
-    styled Html.Styled.input
-        [ display block
-        , Css.width <| px 260
-        , padding2 (px 12) <| px 20
-        , margin2 (px 8) <| px 0
-        , border <| px 0
-        , borderRadius <| px 4
-        ]
-
-
-styledButton : List (Attribute msg) -> List (Html msg) -> Html msg
-styledButton =
-    styled Html.Styled.button
-        [ Css.width <| px 300
-        , backgroundColor <| hex "#397cd5"
-        , color <| hex "#fff"
-        , padding2 (px 12) <| px 20
-        , marginTop <| px 10
-        , border <| px 0
-        , borderRadius <| px 4
-        , fontSize <| px 16
-        ]
+--styledForm : List (Attribute msg) -> List (Html msg) -> Html msg
+--styledForm =
+--    styled Html.Styled.form
+--        [ borderRadius <| px 5
+--        , backgroundColor <| hex "#f2f2f2"
+--        , padding <| px 20
+--        , Css.width <| px 300
+--        ]
+--
+--
+--styledInput : List (Attribute msg) -> List (Html msg) -> Html msg
+--styledInput =
+--    styled Html.Styled.input
+--        [ display block
+--        , Css.width <| px 260
+--        , padding2 (px 12) <| px 20
+--        , margin2 (px 8) <| px 0
+--        , border <| px 0
+--        , borderRadius <| px 4
+--        ]
+--
+--
+--styledButton : List (Attribute msg) -> List (Html msg) -> Html msg
+--styledButton =
+--    styled Html.Styled.button
+--        [ Css.width <| px 300
+--        , backgroundColor <| hex "#397cd5"
+--        , color <| hex "#fff"
+--        , padding2 (px 12) <| px 20
+--        , marginTop <| px 10
+--        , border <| px 0
+--        , borderRadius <| px 4
+--        , fontSize <| px 16
+--        ]
 
 
 init : User
@@ -143,7 +143,7 @@ main : Program () User Msg
 main =
     Browser.sandbox
         { init = initialModel
-        , view = \model -> toUnstyled <| view model
+        , view = view
         , update = update
         }
 
