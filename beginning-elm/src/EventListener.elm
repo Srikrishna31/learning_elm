ecfc33b31c06652e4df3208f62b94a815668c245
Krishna Addepalli <coolkrishna31@gmail.com>
2025-01-24 20:40:35 +0100

    Add keyboard and mouse subscriptions

--- beginning-elm/src/EventListener.elm
+++ beginning-elm/src/EventListener.elm
@@ -1,7 +1,7 @@
 module EventListener exposing (..)
 
 import Browser
-import Browser.Events exposing (onKeyPress)
+import Browser.Events exposing (onClick, onKeyPress)
 import Html exposing (Html, div, text)
 import Json.Decode as Decode
 
@@ -22,19 +22,65 @@ view model =
 
 
 type Msg
-    = KeyPressed
+    = CharacterKey Char
+    | ControlKey String
+    | MouseClick
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        KeyPressed ->
+        CharacterKey 'i' ->
             ( model + 1, Cmd.none )
+
+        CharacterKey 'd' ->
+            ( model - 1, Cmd.none )
+
+        MouseClick ->
+            ( model + 5, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
+
+
+
+{-
+   When we have more than one subscription, we must batch them with Sub.batch:
+
+       Sub.batch : List (Sub msg) -> Sub msg
+-}
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    onKeyPress <| Decode.succeed KeyPressed
+    Sub.batch
+        [ onKeyPress keyDecoder
+        , onClick <| Decode.succeed MouseClick
+        ]
+
+
+keyDecoder : Decode.Decoder Msg
+keyDecoder =
+    Decode.map toKey <| Decode.field "key" Decode.string
+
+
+
+{-
+   # String.uncons
+   The String.uncons function splits a non-empty string into its head and tail.
+
+       uncons: String -> Maybe (Char, String)
+-}
+
+
+toKey : String -> Msg
+toKey keyValue =
+    case String.uncons keyValue of
+        Just ( char, "" ) ->
+            CharacterKey char
+
+        _ ->
+            ControlKey keyValue
 
 
 main : Program () Model Msg
