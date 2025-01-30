2500bce25a1e8785718798591b4830031e1dba92
Krishna Addepalli <coolkrishna31@gmail.com>
2025-01-30 06:21:54 +0100

    Restore the ability to comment on the photos

--- pic-share/src/Picshare.elm
+++ pic-share/src/Picshare.elm
@@ -14,9 +14,9 @@ type alias Id =
 
 
 type Msg
-    = ToggleLike
-    | UpdateComment String
-    | SaveComment
+    = ToggleLike Id
+    | UpdateComment Id String
+    | SaveComment Id
     | LoadFeed (Result Http.Error Feed)
 
 
@@ -70,10 +70,10 @@ viewDetailedPhoto photo =
 
 
 viewLoveButton : Photo -> Html Msg
-viewLoveButton model =
+viewLoveButton photo =
     let
         buttonClass =
-            if model.liked then
+            if photo.liked then
                 "fa-heart"
 
             else
@@ -83,8 +83,7 @@ viewLoveButton model =
         [ i
             [ class "fa fa-2x"
             , class buttonClass
-
-            --, onClick ToggleLike
+            , onClick <| ToggleLike photo.id
             ]
             []
         ]
@@ -115,13 +114,12 @@ viewComments : Photo -> Html Msg
 viewComments photo =
     div []
         [ viewCommentList photo.comments
-        , form [ class "new-comment", onSubmit SaveComment ]
+        , form [ class "new-comment", onSubmit <| SaveComment photo.id ]
             [ input
                 [ type_ "text"
                 , placeholder "Add a comment..."
                 , value photo.newComment
-
-                --, onInput UpdateComment
+                , onInput <| UpdateComment photo.id
                 ]
                 []
             , button [ disabled <| String.isEmpty photo.newComment ]
@@ -218,21 +216,19 @@ in one direction from model to view to messages to update and back to model.
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        --ToggleLike ->
-        --    ( { model | photo = updateFeed toggleLike model.photo }, Cmd.none )
-        --
-        --UpdateComment comment ->
-        --    ( { model | photo = updateFeed (updateComment comment) model.photo }, Cmd.none )
-        --
-        --SaveComment ->
-        --    ( { model | photo = updateFeed saveNewComment model.photo }, Cmd.none )
+        ToggleLike id ->
+            ( { model | feed = updateFeed toggleLike id model.feed }, Cmd.none )
+
+        UpdateComment id comment ->
+            ( { model | feed = updateFeed (updateComment comment) id model.feed }, Cmd.none )
+
+        SaveComment id ->
+            ( { model | feed = updateFeed saveNewComment id model.feed }, Cmd.none )
+
         LoadFeed (Ok feed) ->
             ( { model | feed = Just feed }, Cmd.none )
 
         LoadFeed (Err _) ->
-            ( model, Cmd.none )
-
-        _ ->
             ( model, Cmd.none )
 
 
@@ -246,9 +242,22 @@ updateComment comment photo =
     { photo | newComment = comment }
 
 
-updateFeed : (Photo -> Photo) -> Maybe Photo -> Maybe Photo
-updateFeed updatePhoto maybePhoto =
-    Maybe.map updatePhoto maybePhoto
+updatePhotoById : (Photo -> Photo) -> Id -> Feed -> Feed
+updatePhotoById updatePhoto id feed =
+    List.map
+        (\photo ->
+            if photo.id == id then
+                updatePhoto photo
+
+            else
+                photo
+        )
+        feed
+
+
+updateFeed : (Photo -> Photo) -> Id -> Maybe Feed -> Maybe Feed
+updateFeed updatePhoto id maybeFeed =
+    Maybe.map (updatePhotoById updatePhoto id) maybeFeed
 
 
 saveNewComment : Photo -> Photo
